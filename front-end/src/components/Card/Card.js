9f40a16c04ddaf21f728e89ba51b46bfe1de9c57
Matheus <mgoispaiva@gmail.com>
2022-10-11 20:04:24 -0300

    Atualização - tela de login

--- front-end/src/components/Card/Card.js
+++ front-end/src/components/Card/Card.js
@@ -1,13 +1,16 @@
 import './card.css'
+import { useState } from 'react'
 
 
 
 function Card(props){
 
+  const [nome, setNome] = useState("")
+
     return (
         <div className="card">
             <img src="envolver.png" className="card__image" ></img>
-            <label className="card__titulo" >Envolver</label>
+            <label className="card__titulo" ></label>
             <label className="card__descricao">Single by anita - 2021</label>
         </div>
     )
