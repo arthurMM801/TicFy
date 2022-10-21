c178478aa144df8b5127c8980819918b343a7f02
Matheus <mgoispaiva@gmail.com>
2022-10-21 15:08:22 -0300

    gostos terminado

--- front-end/src/pages/Gostos/index.js
+++ front-end/src/pages/Gostos/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import { useRef, useState } from "react"
 import './gostos.css'
 
 
@@ -8,41 +8,111 @@ const Gostos =()=>{
         id:1,
         descricao: 'Indie',
         img: 'https://reviewzeiro.files.wordpress.com/2014/07/arctic-monkeys-am-cover.jpg?w=317&h=317'
-    }]
+    },
+    {
+        id:2,
+        descricao:"Rock",
+        img:"https://static.preparaenem.com/2021/06/rock-and-roll.jpg?w=200"
+    },
+    {
+        id:3,
+        descricao:"Funk",
+        img:"https://cdns-images.dzcdn.net/images/artist/147fa03566c6fff84fb84b088f17dc6a/264x264.jpg?w=200"
+    },
+    {
+        id:4,
+        descricao:"Pagode",
+        img:"https://www.blogodorium.com.br/wp-content/uploads/pagode.jpg"
+    },
+    {
+        id:5,
+        descricao:"Pop",
+        img:"https://img.freepik.com/vetores-premium/letras-de-vetor-3d-vintage-de-musica-pop-tipo-de-fonte-em-negrito-retro-texto-estilizado-de-arte-pop-letras-de-estilo-antigo-numeros-simbolos-pacote-de-elementos-banner-de-poster-dos-anos-90-80-fundo-de-cor-de-pinho_106317-21305.jpg?w=200"
+    },
+    {
+        id:6,
+        descricao:"Rap",
+        img:"https://img.freepik.com/vetores-gratis/palavra-de-neon-de-rap-e-microfone-no-contorno-de-chama_1262-11901.jpg?w=200"
+    },
+    {
+        id:7,
+        descricao:"Clásica",
+        img:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-165Hu8QNYxwnFyFGFu471iDELzga8pwKfw&usqp=CAU?w=200"
+    },
+]
 
+    const [btnSelect, setbtnSelect] = useState(false)
+
+
+
+const carrocel = useRef(null)
+const item = useRef(null)
+
+const handleLeftClick =(e)=>{
+    e.preventDefault()
+    carrocel.current.scrollLeft -= carrocel.current.offsetWidth
+
+}
+const handleRightClick =(e)=>{
+    e.preventDefault()
+    carrocel.current.scrollLeft += carrocel.current.offsetWidth
+}
+
+const handleCLickItem =(e)=>{
+    e.preventDefault();
+    setbtnSelect(btnSelect => !btnSelect)
+}
+let addClass = btnSelect ? ' active' : ''
+let count = 0 //pra contar os itens
 
     return(
-
         <div className="Container-Gostos">
-            <h1>Escolha seus gêneros musicais preferidos</h1>
+            <h1>Selecione seus gostos musicais preferidos</h1>
 
-            <button className="arrow-left">
+            <div className="carrocel-wrapper" ref={carrocel}>
+
+
+            <button 
+            className={`arrow-left`}
+            onClick={handleLeftClick}
+            >
                 <svg xmlns="http://www.w3.org/2000/svg"
-                 width="24" 
-                 height="24" 
+                 width="65" 
+                 height="65" 
                  viewBox="0 0 24 24">
                     <path d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z"/>
                 </svg>
                 </button>
 
-                <button className="arrow-right">
+                <button 
+                className={`arrow-right`}
+                onClick={handleRightClick}
+                >
                 <svg xmlns="http://www.w3.org/2000/svg"
-                 width="24" 
-                 height="24" 
+                 width="65" 
+                 height="65" 
                  viewBox="0 0 24 24">
                     <path d="M8.122 24l-4.122-4 8-8-8-8 4.122-4 11.878 12z"/>
                 </svg>
                 </button>
 
-            <div className="items-wrapper">
 
                <div className="gallery">
                     {
                         imagens.map(img => {
                             return(
-                                <div key={img.id} className='item'>
-                                    <img src={img.img} alt = {img.descricao}/>
+                                <div 
+                                key={img.id} 
+                                className={`item item${count++}${addClass}`}
+                                ref={item}
+                                onClick={()=>{}}
+                                >
+                                    <img 
+                                    src={img.img} 
+                                    alt = {img.descricao}
+                                    />
                                     <h2>{img.descricao}</h2>
+                                    
                                 </div>
                             )
                         })
--- front-end/src/routes/router.js
+++ front-end/src/routes/router.js
@@ -6,23 +6,20 @@ import Gostos from '../pages/Gostos'
 import Explorar from '../pages/Explorar'
 import Player from '../pages/Player'
 
-//>>>>>>> fc0d42027bc94592effe1852591c993f18a465a5
 
-// ARTHUR O QUE É ISSO, se queser comentar usa o //
 
 
 const Rotas = () =>{
   return(
     <BrowserRouter>
     <Routes>
-      <Route path='/home' element={ <Home/> }/>
+      <Route path='/' element={ <Home/> }/>
       <Route path='/login' element={ <Login/> }/>
 
-      <Route path='SeusGostos' element={ <Gostos/> }/>
+      <Route path='/seusGostos' element={ <Gostos/> }/>
 
       <Route path='/explorar' element={ <Explorar />} />
-      <Route path='/' element={ <Player />} />
-      {/* fc0d42027bc94592effe1852591c993f18a465a5 */}
+      <Route path='/player' element={ <Player />} />
     </Routes>
   </BrowserRouter>
   )
