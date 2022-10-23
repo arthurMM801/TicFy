4fa89336c1574c397650a6adec0be80130b96e3d
Gustavo Herbster <gustavoherbstermacedo@gmail.com>
2022-10-23 18:17:54 -0300

    Ajuste de foto do perfil

--- front-end/src/pages/Perfil/index.js
+++ front-end/src/pages/Perfil/index.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import Cabecalho from "../../components/Cabecalho/Cabecalho"
 import ListaDeMusica from '../../components/ListaDeMusicas/ListaDeMusica'
 import Card from '../../components/Card/Card';
+import Foto from '../../assets/foto_perfil.png'
 
 
 
@@ -14,7 +15,7 @@ function Perfil() {
 
       <div className='perfil'>
         <div className='perfil-wrapper'>
-        <img src="image.png" className="perfil__image" ></img>
+        <img src={Foto} className="perfil__image" ></img>
         <button type="submit" className="edit_btn">
         Editar perfil
         </button>
